613949c346059b0936766be62109a0b87feedb6a
Werka <69664@student.ue.poznan.pl>
2022-05-27 21:22:13 +0200

    halo

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import csv
 
 if __name__ == "__main__":
-    pass
+    calls_dict_sum = dict()
+
+    with open('phoneCalls.csv', 'r') as fin:
+        reader = csv.reader(fin, delimiter=",")
+        headers = next(reader)
+
+        for row in reader:
+            from_subsr = int(row[0])
+            if from_subsr not in calls_dict_sum:
+                calls_dict_sum[from_subsr] = 0
+            calls_dict_sum[from_subsr] += 1
+
+    najczesciej_call = max(calls_dict_sum.items(), key=lambda x: x[1])
+
+    print(najczesciej_call)
+    
